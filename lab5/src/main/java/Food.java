c8589fef0b8ef0f15d3f18e535129fba14342f98
Sandra <sandra.giermanska@gmail.com>
2018-12-01 16:07:30 +0100

    usuniecie

--- lab5/src/main/java/Food.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by student on 2018-10-30.
- */
-public class Food {
-}
--- lab5/src/main/java/InadequateFoodException.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by student on 2018-10-30.
- */
-public class InadequateFoodException extends RuntimeException {
-    InadequateFoodException(String msg) { super(msg); }
-}
