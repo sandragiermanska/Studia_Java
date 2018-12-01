40c5967604146323f784dcdc0637ac01f82b1a06
Sandra <sandra.giermanska@gmail.com>
2018-12-01 16:27:24 +0100

    lab6 - histogram

---
+++ Lab06/Histogram/src/sample/Controller.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField dodajArgument;
+
+    @FXML
+    private TextField dodajWartosc;
+
+    @FXML
+    private BarChart wykres;
+    XYChart.Series set1 = new XYChart.Series<>();
+
+
+    public void pressButtonDodaj(ActionEvent event) {
+        set1.getData().add(new XYChart.Data(dodajArgument.getText(), Integer.parseInt(dodajWartosc.getText())));
+    }
+
+
+    public void pressButtonWyjdz(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        wykres.getData().add(set1);
+    }
+}
