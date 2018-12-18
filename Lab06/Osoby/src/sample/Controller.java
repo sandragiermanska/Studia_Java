3db32e9c26de8dfc4d35e12eaa2793fdcab75f7c
Sandra <sandra.giermanska@gmail.com>
2018-12-18 21:04:34 +0100

    interfejs graficzny - zadanie osoby koniec

--- Lab06/Osoby/src/sample/Controller.java
+++ Lab06/Osoby/src/sample/Controller.java
@@ -4,14 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class Controller {
 
